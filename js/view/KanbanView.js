a03d78214aef040a947f1a6b0593b03bc0a777df
dennis <ssembatyadennis25@gmail.com>
2025-11-11 09:34:43 +0300

    feat: Implement KanbanView class for DOM element binding and event handling

---
+++ js/view/KanbanView.js
@@ -0,0 +1,65 @@
+/**
+ * @file KanbanView.js
+ * @description - The KanbanView.js file is responsible for binding DOM elements with their
+ * respective handlers
+ */
+
+export default class KanbanView {
+  constructor() {
+    // important DOM nodes
+    this.allTasksElementsContainer = document.querySelectorAll(".tasks");
+    this.modalWindow = document.querySelector(".confirm-model");
+    this.columnsContainer = document.querySelector(".columns");
+    this.columns = document.querySelectorAll(".column");
+    this.columnLastElement = document.querySelector(".column").lastElementChild;
+    this.columns = document.querySelectorAll(".column");
+    this.previewTextElement = document.querySelector(".preview");
+    this.tasksContainer = document.querySelector(".tasks");
+    this.draggedTask = document.querySelector(".dragging");
+
+    // buttons
+    this.cancelButton = document.querySelector("#cancel");
+
+    //place holder tasks
+    this.placeholderTasks = [
+      ["Write Reports", "Code Review", "Team meeting"],
+      ["Morning Workouts", "Chill time"],
+      ["Fix Bugs", "Submit Projext", "meeting notes"],
+    ];
+  }
+
+  //////////////// Modal window button Event Listeners
+
+  onConfirmDeleteButtonClick = (handler) => {
+    // confirm deletion
+    this.modalWindow.addEventListener("submit", handler);
+  };
+
+  onCancelButtonClick = (handler) => {
+    // cancel deletion
+    this.cancelButton.addEventListener("click", handler);
+  };
+
+  // clear current task
+  onModalWindowClose = (handler) => {
+    this.modalWindow.addEventListener("close", handler);
+  };
+
+  // Tasks card EventListeners
+
+  onTaskActionButtonsClick = (handler) => {
+    this.columnsContainer.addEventListener("click", handler);
+  };
+
+  onTaskDragOver = () => {
+    for (const taskElement of this.allTasksElementsContainer) {
+      taskElement.addEventListener("dragover", handleDragover);
+    }
+  };
+
+  onTaskDrop = () => {
+    for (const taskElement of this.allTasksElementsContainer) {
+      taskElement.addEventListener("drop", handleDrop);
+    }
+  };
+}
